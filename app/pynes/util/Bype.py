c29a29acad2d9e8b16627a3aeff1bed68924df42
PaoPaoDev <125128258+Paopun20@users.noreply.github.com>
2025-11-20 08:43:03 +0700

    Update Bype.py
    Fix auto check in vs code extension

--- app/pynes/util/Bype.py
+++ app/pynes/util/Bype.py
@@ -1,11 +1,14 @@
 from collections.abc import Callable
-from typing import Union, Any, Type, Tuple, overload
+from typing import Union, Any, Type, Tuple, Optional, TypeVar, cast
 from enum import Enum
 
 
 class Sign(Enum):
     UNSIGNED = "unsigned"
     SIGNED = "signed"
+
+
+T = TypeVar('T', bound='Bype')
 
 
 class BypeMeta(type):
@@ -27,20 +30,20 @@ class BypeMeta(type):
         name = f"Bype_{bits}_{sign.name}"
 
         # Dynamically create subclass
-        subclass: Type["Bype"] = type(name, (Bype,), {"bits": bits, "sign": sign})
+        subclass: Type[Bype] = cast(Type[Bype], type(name, (Bype,), {"bits": bits, "sign": sign}))
 
         # Make subclass callable to auto-wrap int
-        original_init: Callable = subclass.__init__
+        original_init: Callable[..., None] = subclass.__init__
 
-        def new_init(self, value: int) -> None:
+        def new_init(self: Bype, value: int) -> None:
             original_init(self, value)
 
-        subclass.__init__ = new_init
+        subclass.__init__ = new_init  # type: ignore[method-assign]
         return subclass
 
 
 class Bype(metaclass=BypeMeta):
-    bits: int = None
+    bits: Optional[int] = None
     sign: Sign = Sign.SIGNED
 
     def __init__(self, value: int) -> None:
@@ -52,13 +55,14 @@ class Bype(metaclass=BypeMeta):
     def _wrap(self, v: int) -> int:
         v &= self.mask
         if self.sign == Sign.SIGNED:
+            assert self.bits is not None  # For type checker
             sign_bit: int = 1 << (self.bits - 1)
             if v & sign_bit:
                 v -= 1 << self.bits
         return v
 
-    def _new(self, v: int) -> "Bype":
-        return type(self)(v)
+    def _new(self: T, v: int) -> T:
+        return type(self)(v)  # type: ignore[return-value]
 
     # Numeric conversions
     def __int__(self) -> int:
@@ -81,42 +85,43 @@ class Bype(metaclass=BypeMeta):
 
     # Representation
     def __repr__(self) -> str:
-        return f"Bype[{self.bits}, {self.sign.name}]({self.value})"
+        sign_name = self.sign.name if self.sign else "SIGNED"
+        return f"Bype[{self.bits}, {sign_name}]({self.value})"
 
     # Unary
-    def __neg__(self) -> "Bype":
+    def __neg__(self: T) -> T:
         return self._new(-self.value)
 
-    def __pos__(self) -> "Bype":
+    def __pos__(self: T) -> T:
         return self._new(+self.value)
 
-    def __abs__(self) -> "Bype":
+    def __abs__(self: T) -> T:
         return self._new(abs(self.value))
 
-    def __invert__(self) -> "Bype":
+    def __invert__(self: T) -> T:
         return self._new(~self.value)
 
     # Arithmetic
-    def __add__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __add__(self: T, other: Union[int, "Bype"]) -> T:
         return self._new(self.value + int(other))
 
     __radd__ = __add__
 
-    def __sub__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __sub__(self: T, other: Union[int, "Bype"]) -> T:
         return self._new(self.value - int(other))
 
-    def __rsub__(self, other: int) -> "Bype":
+    def __rsub__(self: T, other: int) -> T:
         return self._new(int(other) - self.value)
 
-    def __mul__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __mul__(self: T, other: Union[int, "Bype"]) -> T:
         return self._new(self.value * int(other))
 
     __rmul__ = __mul__
 
-    def __floordiv__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __floordiv__(self: T, other: Union[int, "Bype"]) -> T:
         return self._new(self.value // int(other))
 
-    def __rfloordiv__(self, other: int) -> "Bype":
+    def __rfloordiv__(self: T, other: int) -> T:
         return self._new(int(other) // self.value)
 
     def __truediv__(self, other: Union[int, "Bype"]) -> float:
@@ -125,50 +130,60 @@ class Bype(metaclass=BypeMeta):
     def __rtruediv__(self, other: int) -> float:
         return int(other) / self.value
 
-    def __mod__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __mod__(self: T, other: Union[int, "Bype"]) -> T:
         return self._new(self.value % int(other))
 
-    def __rmod__(self, other: int) -> "Bype":
+    def __rmod__(self: T, other: int) -> T:
         return self._new(int(other) % self.value)
 
-    def __pow__(self, other: Union[int, "Bype"], modulo: Any = None) -> "Bype":
+    def __pow__(self: T, other: Union[int, "Bype"], modulo: Optional[int] = None) -> T:
         if modulo is None:
             return self._new(pow(self.value, int(other)))
         return self._new(pow(self.value, int(other), modulo))
 
-    def __rpow__(self, other: int) -> "Bype":
+    def __rpow__(self: T, other: int) -> T:
         return self._new(pow(int(other), self.value))
 
     # Comparisons
     def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, (int, Bype)):
+            return NotImplemented
         return self.value == int(other)
 
     def __lt__(self, other: Any) -> bool:
+        if not isinstance(other, (int, Bype)):
+            return NotImplemented
         return self.value < int(other)
 
     def __le__(self, other: Any) -> bool:
+        if not isinstance(other, (int, Bype)):
+            return NotImplemented
         return self.value <= int(other)
 
     def __gt__(self, other: Any) -> bool:
+        if not isinstance(other, (int, Bype)):
+            return NotImplemented
         return self.value > int(other)
 
     def __ge__(self, other: Any) -> bool:
+        if not isinstance(other, (int, Bype)):
+            return NotImplemented
         return self.value >= int(other)
 
     # Bitwise
-    def __and__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __and__(self: T, other: Union[int, "Bype"]) -> T:
         return self._new(self.value & int(other))
 
-    def __or__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __or__(self: T, other: Union[int, "Bype"]) -> T:
         return self._new(self.value | int(other))
 
-    def __xor__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __xor__(self: T, other: Union[int, "Bype"]) -> T:
         return self._new(self.value ^ int(other))
 
-    def __lshift__(self, other: int) -> "Bype":
+    def __lshift__(self: T, other: int) -> T:
         return self._new(self.value << other)
 
-    def __rshift__(self, other: int) -> "Bype":
+    def __rshift__(self: T, other: int) -> T:
         return self._new(self.value >> other)
 
     __rand__ = __and__
@@ -176,53 +191,53 @@ class Bype(metaclass=BypeMeta):
     __rxor__ = __xor__
 
     # In-place operations
-    def __iadd__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __iadd__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(self.value + int(other))
         return self
 
-    def __isub__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __isub__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(self.value - int(other))
         return self
 
-    def __imul__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __imul__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(self.value * int(other))
         return self
 
-    def __ifloordiv__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __ifloordiv__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(self.value // int(other))
         return self
 
-    def __itruediv__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __itruediv__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(int(self.value / int(other)))
         return self
 
-    def __imod__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __imod__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(self.value % int(other))
         return self
 
-    def __ipow__(self, other: Union[int, "Bype"], modulo: Any = None) -> "Bype":
+    def __ipow__(self: T, other: Union[int, "Bype"], modulo: Optional[int] = None) -> T:
         if modulo is None:
             self.value = self._wrap(pow(self.value, int(other)))
         else:
             self.value = self._wrap(pow(self.value, int(other), modulo))
         return self
 
-    def __iand__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __iand__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(self.value & int(other))
         return self
 
-    def __ior__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __ior__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(self.value | int(other))
         return self
 
-    def __ixor__(self, other: Union[int, "Bype"]) -> "Bype":
+    def __ixor__(self: T, other: Union[int, "Bype"]) -> T:
         self.value = self._wrap(self.value ^ int(other))
         return self
 
-    def __ilshift__(self, other: int) -> "Bype":
+    def __ilshift__(self: T, other: int) -> T:
         self.value = self._wrap(self.value << other)
         return self
 
-    def __irshift__(self, other: int) -> "Bype":
+    def __irshift__(self: T, other: int) -> T:
         self.value = self._wrap(self.value >> other)
         return self
