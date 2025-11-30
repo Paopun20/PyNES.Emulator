94a1bf592ee601d2b307ad14d7d8c615268a9738
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2025-11-30 20:11:20 +0700

    remove debug

--- app/resources.py
+++ app/resources.py
@@ -6,11 +6,3 @@ font_path: Path = assets_path / "fonts" / "Tiny5.ttf"
 icon_path: Path = root_path / "icon.ico"
 shader_path: Path = assets_path / "shaders"
 config_file: Path = root_path / "config.toml"
-
-if __name__ == "__main__" or True:
-    print(root_path)
-    print(assets_path)
-    print(font_path)
-    print(icon_path)
-    print(shader_path)
-    print(config_file)
